87863f602e872f4167c8c79606dca34cffd45b1f
devtom <tom.anthonie@indexpeople.nl>
2019-07-19 18:39:30 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -10,11 +10,20 @@ function App() {
     <BrowserRouter>
     <div className="App">
       <Nav />
+      <Switch>
+      <Route path="/" exact component={Home} />
       <Route path="/about" component={About} />
       <Route path="/shop" component={Shop} />
+      </Switch>
     </div>
     </BrowserRouter>
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+)
 
 export default App;
